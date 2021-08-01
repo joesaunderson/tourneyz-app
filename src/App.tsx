f92832ec6b1b6070ddfd7f358ba35f6ad22d971b
Joe Saunderson <joe.saunderson@mention-me.com>
2021-08-01 12:52:41 +0100

    Upgrade Yarn version & some general foundations

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import {Route, Switch} from "react-router-dom";
+import {ApolloProvider} from "@apollo/client";
+import client from "./apollo/client";
+import Login from "./pages/Login/Login";
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <ApolloProvider client={client}>
+            <Switch>
+                <Route path="/login">
+                    <Login />
+                </Route>
+            </Switch>
+        </ApolloProvider>
+    );
 }
 
 export default App;
---
+++ src/pages/Login/Login.tsx
@@ -0,0 +1,12 @@
+interface Props {
+}
+
+const Login: React.VoidFunctionComponent<Props> = () => {
+    return (
+        <span>
+            This is the Login component
+        </span>
+    );
+};
+
+export default Login;
