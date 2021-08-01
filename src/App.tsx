a1351e94d6f210cc30043ac29c879f7253bead92
Joe Saunderson <joe.saunderson@mention-me.com>
2021-08-01 14:24:55 +0100

    Install and run prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
 import React from "react";
 import "./App.css";
 
-import { Route, Switch } from "react-router-dom";
-import { ApolloProvider } from "@apollo/client";
+import {Route, Switch} from "react-router-dom";
+import {ApolloProvider} from "@apollo/client";
 import client from "./apollo/client";
 import Login from "./pages/Login/Login";
 import Error404 from "./pages/Error404/Error404";
 
 function App() {
-  return (
-    <ApolloProvider client={client}>
-      <Switch>
-        <Route path="/login">
-          <Login />
-        </Route>
-        <Route path="*">
-          <Error404 />
-        </Route>
-      </Switch>
-    </ApolloProvider>
-  );
+    return (
+        <ApolloProvider client={client}>
+            <Switch>
+                <Route path="/login">
+                    <Login/>
+                </Route>
+                <Route path="*">
+                    <Error404/>
+                </Route>
+            </Switch>
+        </ApolloProvider>
+    );
 }
 
 export default App;
