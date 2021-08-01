e888e267947d4bff95a35f2b1e473ff1aa65ae1f
Joe Saunderson <joe.saunderson@mention-me.com>
2021-08-01 14:10:41 +0100

    Add basic 404

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,17 @@ import {Route, Switch} from "react-router-dom";
 import {ApolloProvider} from "@apollo/client";
 import client from "./apollo/client";
 import Login from "./pages/Login/Login";
+import Error404 from "./pages/Error404/Error404";
 
 function App() {
     return (
         <ApolloProvider client={client}>
             <Switch>
                 <Route path="/login">
-                    <Login />
+                    <Login/>
+                </Route>
+                <Route path="*">
+                    <Error404/>
                 </Route>
             </Switch>
         </ApolloProvider>
